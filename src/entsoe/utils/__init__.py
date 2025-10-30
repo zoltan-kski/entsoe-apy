aa55b7511a6042045671d990f1ed08002b0d3f65
BerriJ <37799358+BerriJ@users.noreply.github.com>
2025-10-30 09:52:08 +0000

    Style fixes by ruff

--- src/entsoe/utils/__init__.py
+++ src/entsoe/utils/__init__.py
@@ -1,5 +1,5 @@
-from .records import extract_records
 from .mappings_dict import mappings
+from .records import extract_records
 from .timestamps import add_timestamps, calculate_timestamp
 
 __all__ = ["mappings", "extract_records", "add_timestamps", "calculate_timestamp"]
